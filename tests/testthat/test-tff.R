35fdfb2fc8afc0d59afc401bf0e94b96084a3d7c
Timothy Mastny <tim.mastny@gmail.com>
2020-08-05 10:42:22 -0500

    working tests

--- tests/testthat/test-tff.R
+++ tests/testthat/test-tff.R
@@ -1,13 +1,25 @@
 library(recipes)
 
 test_that("step_tff creates new columns", {
+  prices <- tsrecipes::prices
 
+  prices_xf <- recipe(prices) %>%
+    step_fft(ts) %>%
+    prep() %>%
+    bake(prices)
+
+  expect_equal(dim(prices_xf), c(965, 10))
 })
 
-prices <- tsrecipes::prices
-prices
 
-recipe(prices) %>%
-  step_fft(ts) %>%
-  prep() %>%
-  bake(prices)
+test_that("step_tff handles multiple time series", {
+  prices <- tsrecipes::prices %>%
+    mutate(ts2 = ts)
+
+  prices_xf <- recipe(prices) %>%
+    step_fft(starts_with("ts")) %>%
+    prep() %>%
+    bake(prices)
+
+  expect_equal(dim(prices_xf), c(965, 18))
+})
