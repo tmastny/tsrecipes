baa999b8b8ebe4453576d88704f03efc04a92a7b
Timothy Mastny <tim.mastny@gmail.com>
2020-08-09 14:00:26 -0500

    reconstruct

---
+++ tests/testthat/test-reconstruct.R
@@ -0,0 +1,16 @@
+test_that("reconstruct has same length as original time series", {
+  prices <- tsrecipes::prices
+
+  trained_rec <- recipe(prices) %>%
+    step_dct(ts) %>%
+    prep()
+
+  prices_xf <- bake(trained_rec, prices)
+
+  check_length <- prices_xf %>%
+    reconstruct("ts", trained_rec$steps[[1]], starts_with("ts.")) %>%
+    rowwise() %>%
+    mutate(recon_same_length = length(ts_recon) == 60)
+
+  expect_true(all(check_length$recon_same_length))
+})
