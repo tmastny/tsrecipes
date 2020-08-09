e5cbb326088644ff35153e24cf96bba247af423a
Timothy Mastny <tim.mastny@gmail.com>
2020-08-09 14:03:20 -0500

    prices doc

--- R/data.R
+++ R/data.R
@@ -1,12 +1,13 @@
-#' Prices of 50,000 round cut diamonds.
+#' Share Price Increase
 #'
 #' A dataset containing the prices and other attributes of almost 54,000
 #' diamonds.
 #'
 #' @format A data frame with 53940 rows and 10 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{id}{unique id of the time series}
+#'   \item{class}{whether or not the time series increased or not}
+#'   \item{ts}{list column, containing a time series of length 60 for each row}
 #'   ...
 #' }
 #' @source \url{http://www.timeseriesclassification.com/description.php?Dataset=SharePriceIncrease}
