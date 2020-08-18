25516b8eae267d5009527a3aa11b495e8a3fa346
Timothy Mastny <tim.mastny@gmail.com>
2020-08-17 20:05:57 -0500

    update docs

--- R/data.R
+++ R/data.R
@@ -1,9 +1,8 @@
 #' Share Price Increase
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' Time series of price increases over a 60 day period.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 965 rows and 3 variables:
 #' \describe{
 #'   \item{id}{unique id of the time series}
 #'   \item{class}{whether or not the time series increased or not}
@@ -13,17 +12,16 @@
 #' @source \url{http://www.timeseriesclassification.com/description.php?Dataset=SharePriceIncrease}
 "prices"
 
-#' Share Price Increase
+#' Ethanol levels
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' Time series of ethanol with varying levels of alcohol. Four classes.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 504 rows and 3 variables:
 #' \describe{
 #'   \item{id}{unique id of the time series}
-#'   \item{class}{whether or not the time series increased or not}
-#'   \item{ts}{list column, containing a time series of length 60 for each row}
+#'   \item{class}{four classes: e35, e38, e40, e45}
+#'   \item{ts}{list column, containing a time series of length 1751 for each row}
 #'   ...
 #' }
-#' @source \url{# http://www.timeseriesclassification.com/description.php?Dataset=EthanolLevel}
+#' @source \url{http://www.timeseriesclassification.com/description.php?Dataset=EthanolLevel}
 "ethanol"
